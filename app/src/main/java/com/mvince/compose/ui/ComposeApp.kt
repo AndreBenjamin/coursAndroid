de583625fc678cd80fe2c1e6a85ca9706de69f26
MarieTeameSIO <benjamin4andre@gmail.com>
2023-04-25 09:51:59 +0200

    [BAN]

--- app/src/main/java/com/mvince/compose/ui/ComposeApp.kt
+++ app/src/main/java/com/mvince/compose/ui/ComposeApp.kt
@@ -7,8 +7,9 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.mvince.compose.signUp.SignUpScreen
+import com.mvince.compose.ui.signUp.SignUpScreen
 import com.mvince.compose.ui.details.DetailsScreen
+import com.mvince.compose.ui.signUp.SignUpScreen
 import com.mvince.compose.ui.users.UsersScreen
 
 @Composable
